ea6c68b5030628de48191da9d5734927c0896bf5
mamodmohamed <51862436+mamodmohamed@users.noreply.github.com>
2022-07-28 10:36:49 +0200

    Create valid-anagram.php

---
+++ valid-anagram.php
@@ -0,0 +1,31 @@
+class Solution {
+
+    /**
+     * @param String $s
+     * @param String $t
+     * @return Boolean
+     */
+
+    function isAnagram($s, $t) {
+         if (strlen($s)==strlen($t))
+         {
+             $s = str_split($s);
+             sort($s);
+             $s=implode($s);
+             
+             $t = str_split($t);
+             sort($t);
+             $t=implode($t); 
+             
+         }
+        if($s==$t)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+   
+    }
+}
